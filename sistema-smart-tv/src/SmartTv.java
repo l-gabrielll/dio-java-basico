28b78f6b4522517b2e455237938dc29dd16fe62c
Luã <luagabrielleandro@gmail.com>
2025-04-06 15:58:03 -0300

    Primeiras interações com a Smart TV

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,29 @@
+public class SmartTv {
+    boolean ligada = true;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+       volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+    }
+}
---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.desligar();
+        System.out.println("Status2 -> TV ligada? " + smartTv.ligada);
+
+        smartTv.ligar();
+        smartTv.aumentarVolume();
+        System.out.println("Status3 -> TV ligada? " + smartTv.ligada);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarCanal();
+        System.out.println("Status4 -> TV ligada? " + smartTv.ligada);
+        System.out.println("Volume atual: " + smartTv.volume);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.diminuirCanal();
+        System.out.println("Status5 -> TV ligada? " + smartTv.ligada);
+        System.out.println("Volume atual: " + smartTv.volume);
+        System.out.println("Canal atual: " + smartTv.canal);
+    }
+}
