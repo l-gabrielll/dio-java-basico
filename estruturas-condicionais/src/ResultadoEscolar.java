48e1ef212d355fd9a7195f8e5d32592c410ab4dc
Luã <luagabrielleandro@gmail.com>
2025-04-08 14:52:28 -0300

    Adicionando a recuperação na condição ternária

--- estruturas-condicionais/src/ResultadoEscolar.java
+++ estruturas-condicionais/src/ResultadoEscolar.java
@@ -1,8 +1,8 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        double nota = 7;
+        double nota = 4;
 
-        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
         System.out.println(resultado);
     }
 }
