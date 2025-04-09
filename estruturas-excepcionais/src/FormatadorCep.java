e716dcee9a05f6cfdad4000b5bf58285497d0104
Luã <luagabrielleandro@gmail.com>
2025-04-09 15:02:39 -0300

    Criando uma exceção

---
+++ estruturas-excepcionais/src/FormatadorCep.java
@@ -0,0 +1,18 @@
+public class FormatadorCep {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O cep precisa conter 8 números.");
+        }
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+}
