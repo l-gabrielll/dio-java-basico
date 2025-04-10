5d36e81cf18e85527b8f2187ff1fec88233b4d6c
Luã <luagabrielleandro@gmail.com>
2025-04-10 14:24:52 -0300

    feat: add método selecaoCandidatos()

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        selecaoCandidatos();
+        /*Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("informe a quantidade de candidatos: ");
         int quantidadeCandidatos = scanner.nextInt();
@@ -18,15 +19,37 @@ public class ProcessoSeletivo {
         System.out.println(retorno);
         }
 
+        scanner.close();*/
+    }
+
+    public static void selecaoCandidatos(){
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        String candidatos[] = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            System.out.println(candidato + " digite o valor do salário pretendido: ");
+            double salarioPretendido = scanner.nextDouble();
+
+            System.out.println("O salário pretendido de " + candidato + " é de R$ " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println(candidato + " teve sua candidatura aprovada para a vaga.");
+                candidatosSelecionados ++;
+            }
+            candidatoAtual ++;
+        }
         scanner.close();
     }
 
-    public static String analisarCandidato(double salarioCandidato){
+    public static String analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
         String resultado;
-        if (salarioBase > salarioCandidato) {
+        if (salarioBase > salarioPretendido) {
             resultado = "Ligar para o candidato";           
-        } else if (salarioBase == salarioCandidato) {
+        } else if (salarioBase == salarioPretendido) {
             resultado = "Ligar para o candidato com contra proposta";
         } else {
             resultado = "Aguardando resultado dos demais candidatos";
